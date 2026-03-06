0adfae51724a9be64f6bdceddb8fc51377505e0f
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2026-03-06 16:29:44 -0600

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,27 +1,22 @@
-import { encrypt } from "./crypto.js";
-
-async function encode(url){
-
-const encrypted = await encrypt(url);
-
-return btoa(encrypted);
-
-}
-
-window.go = async function(){
-
-let url = document.getElementById("url").value;
-
-if(!url.startsWith("http")) url="https://"+url;
-
-const encoded = await encode(url);
-
-const iframe=document.createElement("iframe");
-
-iframe.src="/proxy?url="+encoded;
-
-document.body.appendChild(iframe);
-
-}
-
 navigator.serviceWorker.register("/sw.js");
+
+function encode(url){
+  return btoa(url);
+}
+
+window.go = function(){
+
+  let url=document.getElementById("url").value;
+
+  if(!url.startsWith("http")) url="https://"+url;
+
+  const encoded=encode(url);
+
+  const iframe=document.createElement("iframe");
+
+  iframe.src="/proxy?url="+encoded;
+
+  document.body.innerHTML="";
+  document.body.appendChild(iframe);
+
+};
